9f4d4fd2cfadd43089e762cf0eae6eca54623fd0
zabop <szabopal96@gmail.com>
2019-01-29 13:54:21 +0100

    updated version, now writes every col out

--- fitstotxt.py
+++ fitstotxt.py
@@ -18,12 +18,25 @@ for EACH in foundfiles:
 
     hdul = fits.open(EACH)
     data=hdul[1].data
-    TIME=data['TIME']
-    FLUX_RAW=data['FLUX_RAW']
 
     foutname=EACH.split('/')[-1]+'.txt'
     fout=open(foutname,'w')
-    fout.write('TIME FLUX_RAW')
-    for index, each in enumerate(TIME):
-        fout.write(str(each)+" "+str(FLUX_RAW[index])+'\n')
+
+    for index, column_name in enumerate(data.names):
+        if index==0:
+            fout.write('#'+column_name)
+        else:
+            fout.write(' '+column_name)
+    fout.write('\n')
+
+    data = [data[each] for each in data.names]
+
+    for rownum in range(len(data[0])):
+        for colnum in range(len(data)):
+            if colnum==0:
+                fout.write(str(data[colnum][rownum]))
+            else:
+                fout.write(' '+str(data[colnum][rownum]))
+        fout.write('\n')
+
     fout.close()
